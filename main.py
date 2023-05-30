32708f2c183408a98880e80953846f95328e583f
Dev Prakash <dev.tiwary5821@gmail.com>
2023-05-31 00:01:57 +0530

    2nd Commit (implementing plot graph part)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import plotly.express as px
 
 st.title("Weather forecast for Day")
 
@@ -8,3 +9,9 @@ date = st.slider("Forecast Days", min_value=1, max_value=5, help="Select the no 
 option = st.selectbox("Select Data to view", ("Temperature", "Sky"))
 
 st.subheader(f"{option} for next {date} Days in {place}")
+
+dates = ["2022-25-10", "2022-26-10", "2022-27-10"]
+temperature = [10, 11, 15]
+
+figure = px.line(x=dates, y=temperature, labels={'x': "Date", 'y': "Temperature"})
+st.plotly_chart(figure)
