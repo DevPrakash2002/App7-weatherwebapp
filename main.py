10c0a308a75057fe86245f4af41683924be7c047
Dev Prakash <dev.tiwary5821@gmail.com>
2023-06-01 19:43:25 +0530

    3rd Commit (implementing plot fake data)

--- main.py
+++ main.py
@@ -9,9 +9,13 @@ date = st.slider("Forecast Days", min_value=1, max_value=5, help="Select the no 
 option = st.selectbox("Select Data to view", ("Temperature", "Sky"))
 
 st.subheader(f"{option} for next {date} Days in {place}")
+def get_data(date):
+    dates = ["2022-25-10", "2022-26-10", "2022-27-10"]
+    temperatures = [10, 11, 15]
+    temperatures = [date * i for i in temperatures]
+    return dates, temperatures
 
-dates = ["2022-25-10", "2022-26-10", "2022-27-10"]
-temperature = [10, 11, 15]
+d, t = get_data(date)
 
-figure = px.line(x=dates, y=temperature, labels={'x': "Date", 'y': "Temperature"})
+figure = px.line(x=d, y=t, labels={'x': "Date", 'y': "Temperature"})
 st.plotly_chart(figure)
