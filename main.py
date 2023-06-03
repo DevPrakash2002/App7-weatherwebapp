f9b0ea684a1e1d7c81cfc3184d472eb3d3051dff
Dev Prakash <dev.tiwary5821@gmail.com>
2023-06-03 12:13:42 +0530

    4th Commit (taking data from api and integrating weather data into function according to requirements)

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
 st.title("Weather forecast for Day")
 
 place = st.text_input("Place")
-date = st.slider("Forecast Days", min_value=1, max_value=5, help="Select the no forecast Days")
+day = st.slider("Forecast Days", min_value=1, max_value=5, help="Select the no forecast Days")
 
 option = st.selectbox("Select Data to view", ("Temperature", "Sky"))
 
-st.subheader(f"{option} for next {date} Days in {place}")
-def get_data(date):
-    dates = ["2022-25-10", "2022-26-10", "2022-27-10"]
-    temperatures = [10, 11, 15]
-    temperatures = [date * i for i in temperatures]
-    return dates, temperatures
+st.subheader(f"{option} for next {day} Days in {place}")
 
-d, t = get_data(date)
+data = get_data(place, day, option)
 
 figure = px.line(x=d, y=t, labels={'x': "Date", 'y': "Temperature"})
 st.plotly_chart(figure)
