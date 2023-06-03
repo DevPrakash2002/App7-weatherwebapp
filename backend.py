d49d070f9937a7f0663ee9675481a92e1fb4ce03
Dev Prakash <dev.tiwary5821@gmail.com>
2023-06-03 15:14:06 +0530

    5th commit (added sky feature)

--- backend.py
+++ backend.py
@@ -3,23 +3,15 @@ import requests
 api_key = '4b6437820089d8b5502b9c87069a9ddd'
 
 
-def get_data(place, forecast_days, kind):
+def get_data(place, forecast_days):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={api_key}"
     response = requests.get(url)
     data = response.json()
     filtered_data = data["list"]
     nr_value = 8 * forecast_days
-
     filtered_data = filtered_data[:nr_value]
-
-    if kind == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    if kind == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
-
-
     return filtered_data
 
 
 if __name__ == "__main__" :
-    print(get_data(place="london", forecast_days=3, kind="Sky"))
+    print(get_data(place="london", forecast_days=3))
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import plotly.express as px
 from backend import get_data
-
+# Adding title, select-box, slider and sub-headers
 st.title("Weather forecast for Day")
 
 place = st.text_input("Place")
@@ -10,8 +10,20 @@ day = st.slider("Forecast Days", min_value=1, max_value=5, help="Select the no f
 option = st.selectbox("Select Data to view", ("Temperature", "Sky"))
 
 st.subheader(f"{option} for next {day} Days in {place}")
+# Get the Temperature or Sky Data
 
-data = get_data(place, day, option)
+if place:
+    filtered_data = get_data(place=place, forecast_days=day)
+# plotting the graph and displaying temperature data received
+if option == "Temperature":
+    temperatures = [dict["main"]["temp"] for dict in filtered_data]
+    date = [dict["dt_txt"] for dict in filtered_data]
+    figure = px.line(x=date, y=temperatures, labels={'x': "Date", 'y': "Temperature"})
+    st.plotly_chart(figure)
 
-figure = px.line(x=d, y=t, labels={'x': "Date", 'y': "Temperature"})
-st.plotly_chart(figure)
+# displaying sky data
+if option == "Sky":
+    temperatures = [dict["weather"][0]["main"] for dict in filtered_data]
+    images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png", "Rain": "images/rain.png", "Snow": "images/snow.png"}
+    image_path = [images[condition] for condition in temperatures]
+    st.image(image_path, width=112)
