ebc70330e83af6635473d89369d5c7b8879692e4
Dev Prakash <dev.tiwary5821@gmail.com>
2023-06-06 20:03:56 +0530

    5th commit (added sky feature)

--- main.py
+++ main.py
@@ -11,19 +11,22 @@ option = st.selectbox("Select Data to view", ("Temperature", "Sky"))
 
 st.subheader(f"{option} for next {day} Days in {place}")
 # Get the Temperature or Sky Data
+try:
+    if place:
+        filtered_data = get_data(place=place, forecast_days=day)
+    # plotting the graph and displaying temperature data received
+    if option == "Temperature":
+        temperatures = [dict["main"]["temp"]/10 for dict in filtered_data]
+        date = [dict["dt_txt"] for dict in filtered_data]
+        figure = px.line(x=date, y=(temperatures), labels={'x': "Date", 'y': "Temperature"})
+        st.plotly_chart(figure)
 
-if place:
-    filtered_data = get_data(place=place, forecast_days=day)
-# plotting the graph and displaying temperature data received
-if option == "Temperature":
-    temperatures = [dict["main"]["temp"] for dict in filtered_data]
-    date = [dict["dt_txt"] for dict in filtered_data]
-    figure = px.line(x=date, y=temperatures, labels={'x': "Date", 'y': "Temperature"})
-    st.plotly_chart(figure)
+    # displaying sky data
+    if option == "Sky":
+        temperatures = [dict["weather"][0]["main"] for dict in filtered_data]
+        images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png", "Rain": "images/rain.png", "Snow": "images/snow.png"}
+        image_path = [images[condition] for condition in temperatures]
+        st.image(image_path, width=112)
 
-# displaying sky data
-if option == "Sky":
-    temperatures = [dict["weather"][0]["main"] for dict in filtered_data]
-    images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png", "Rain": "images/rain.png", "Snow": "images/snow.png"}
-    image_path = [images[condition] for condition in temperatures]
-    st.image(image_path, width=112)
+except KeyError:
+    st.text("PLease enter a valid place")
